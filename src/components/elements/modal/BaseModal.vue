f8622ac06d677ab6f6e6aaf607f63b70a904fc9e
Ilya <garbuzilya.work@gmail.com>
2022-10-11 21:55:34 +0300

    Fix bugs

--- src/components/elements/modal/BaseModal.vue
+++ src/components/elements/modal/BaseModal.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade">
-    <div v-if="open" class="modal-base bg-white">
+    <div v-if="open" class="modal-base">
       <transition name="drop">
         <div class="inner" v-show="open">
           <div class="content bg-white h-full px-4 py-16">
