f7699dc20aebda2a3d3a02929b8d379d89156c69
Ilya <garbuzilya.work@gmail.com>
2022-11-01 20:16:17 +0300

    Fix bugs

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,10 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0 };
+  },
 });
 
 export default router;
